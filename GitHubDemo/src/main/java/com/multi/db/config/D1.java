b3922b7be0c1095f1c4c48cb765fc5e8ac20e814
sagar81297 <sagarchoksi8@gmail.com>
2022-04-02 22:44:30 +0530

    changes by master

--- GitHubDemo/src/main/java/com/multi/db/config/D1.java
+++ GitHubDemo/src/main/java/com/multi/db/config/D1.java
@@ -3,7 +3,7 @@ package com.multi.db.config;
 public class D1 {
 
 	public static void main(String[] args) {
-		System.out.println("D1 class by sagar-1 branch!");
+		System.out.println("D1 class by master branch!");
 	}
 
 }
