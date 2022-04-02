86652368e5ece7358bf378c9f4b0a9dcec9368ff
sagar81297 <78718616+sagar81297@users.noreply.github.com>
2022-04-02 22:16:52 +0530

    Update and rename d1.java to D1.java

--- GitHubDemo/src/main/java/com/multi/db/config/D1.java
+++ GitHubDemo/src/main/java/com/multi/db/config/D1.java
@@ -1,6 +1,6 @@
 package com.multi.db.config;
 
-public class GitHubDemoApplication {
+public class D1 {
 
 	public static void main(String[] args) {
 		System.out.println("d1 class by sagar-3 branch!");
